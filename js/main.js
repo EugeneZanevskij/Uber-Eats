3bcda87e1b8a8c608996833a6c07e636fbe97ced
EugeneZanevskij <zanevskijzenia@gmail.com>
2023-01-18 19:15:38 +0300

    new file:   images/app_store.png 	new file:   images/arrow.svg 	new file:   images/bar_bq.png 	new file:   images/basket.svg 	new file:   images/cinnabon.png 	new file:   images/dimsum.png 	new file:   images/dvizh.png 	new file:   images/google_play.png 	new file:   images/logo.svg 	new file:   images/mcdonalds.png 	new file:   images/nha.png 	new file:   images/pizzelia.png 	new file:   images/rest/photo-1.png 	new file:   images/rest/photo-2.png 	new file:   images/rest/photo-3.png 	new file:   images/rest/photo-4.png 	new file:   images/rest/photo-5.png 	new file:   images/rest/photo.png 	new file:   images/rest/traktir_pushkin.png 	new file:   images/tochka_dzy.png 	new file:   images/white-logo.svg 	new file:   images/world.svg 	new file:   images/zu.png 	new file:   index.html 	new file:   js/main.js 	new file:   restaurant.html 	new file:   styles.css

---
+++ js/main.js
@@ -0,0 +1,39 @@
+/* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        var i;
+        for (i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
+}
+
+// Get the button:
+let mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 200px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+        mybutton.style.display = "block";
+    } else {
+        mybutton.style.display = "none";
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
